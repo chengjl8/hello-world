b34a5df382146a0fc9e8e29237d75aa6ecc4db52
chengjl8 <87250491@qq.com>
2017-03-04 09:39:06 +0800

    增加一个源代码
    硬件描述的获取，VB.NET

---
+++ hard/Form1.Designer.vb
@@ -0,0 +1,92 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(39, 70)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(117, 29)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "硬盘类型"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(39, 129)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(117, 29)
+        Me.Button2.TabIndex = 0
+        Me.Button2.Text = "磁盘控制器序列号"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2})
+        Me.ListView1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.GridLines = True
+        Me.ListView1.Location = New System.Drawing.Point(227, 25)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(430, 249)
+        Me.ListView1.TabIndex = 1
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "参数名"
+        Me.ColumnHeader1.Width = 154
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "结果"
+        Me.ColumnHeader2.Width = 167
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(764, 286)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
+    Friend WithEvents ColumnHeader1 As System.Windows.Forms.ColumnHeader
+    Friend WithEvents ColumnHeader2 As System.Windows.Forms.ColumnHeader
+
+End Class
---
+++ hard/Form1.vb
@@ -0,0 +1,138 @@
+﻿Imports System
+
+Imports System.Management
+
+Public Class Form1
+    Structure INFO_HARDDISK
+        Dim diskId As String          '0
+        Dim diskSerialNumber As String    '1
+        Dim diskModel As String           '1
+        Dim Availability As UInt16
+        Dim Description As String
+        Dim DeviceID As String
+        Dim Manufacturer As String
+        Dim MaxMediaSize As UInt64
+        Dim Name As String
+        Dim size As UInt64
+        Dim TotalCylinders As UInt64
+        Dim TotalHeads As UInt32
+        Dim TotalSectors As UInt64
+        Dim totalTracks As UInt64
+        Dim TracksPerCylinder As UInt32
+
+    End Structure
+    Public hd(1) As INFO_HARDDISK
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim strTemp As String = ""
+        Dim cmicWmi As New ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive")
+   
+        Dim diskId As String = "" '数字ID
+        Dim diskSerialNumber As String = "" '这个我们暂且称其为序列号码
+        Dim diskModel As String = "" '序列号
+        Dim i As Int32
+        i = 0
+        For Each cmicWmiObj As ManagementObject In cmicWmi.Get
+            If i < 2 Then
+                hd(i).Description = cmicWmiObj("Description")
+                hd(i).diskId = cmicWmiObj("signature")
+                hd(i).diskSerialNumber = cmicWmiObj("serialnumber")
+                hd(i).diskModel = cmicWmiObj("Model")
+                hd(i).Availability = cmicWmiObj("Availability")
+                hd(i).DeviceID = cmicWmiObj("DeviceID")
+                hd(i).Manufacturer = cmicWmiObj("Manufacturer")
+                hd(i).MaxMediaSize = cmicWmiObj("MaxMediaSize")
+                hd(i).Name = cmicWmiObj("Name")
+                hd(i).size = cmicWmiObj("size")
+                hd(i).TotalCylinders = cmicWmiObj("TotalCylinders")
+                hd(i).TotalHeads = cmicWmiObj("TotalHeads")
+                hd(i).TotalSectors = cmicWmiObj("TotalSectors")
+                hd(i).totalTracks = cmicWmiObj("totalTracks")
+                hd(i).TracksPerCylinder = cmicWmiObj("TracksPerCylinder")
+            End If
+
+            'diskId = cmicWmiObj("signature")
+            'diskSerialNumber = cmicWmiObj("serialnumber")
+            'diskModel = cmicWmiObj("Model")
+            i += 1
+        Next
+
+       
+
+        '        class Win32_DiskDrive : CIM_DiskDrive
+        '{
+        '  uint16   Availability;
+        '  uint32   BytesPerSector;
+        '  uint16   Capabilities[];
+        '  string   CapabilityDescriptions[];
+        '  string   Caption;
+        '  string   CompressionMethod;
+        '  uint32   ConfigManagerErrorCode;
+        '  boolean  ConfigManagerUserConfig;
+        '  string   CreationClassName;
+        '  uint64   DefaultBlockSize;
+        '  string   Description;
+        '  string   DeviceID;
+        '  boolean  ErrorCleared;
+        '  string   ErrorDescription;
+        '  string   ErrorMethodology;
+        '  string   FirmwareRevision;
+        '  uint32   Index;
+        '  datetime InstallDate;
+        '  string   InterfaceType;
+        '  uint32   LastErrorCode;
+        '  string   Manufacturer;
+        '  uint64   MaxBlockSize;
+        '  uint64   MaxMediaSize;
+        '  boolean  MediaLoaded;
+        '  string   MediaType;
+        '  uint64   MinBlockSize;
+        '  string   Model;
+        '  string   Name;
+        '  boolean  NeedsCleaning;
+        '  uint32   NumberOfMediaSupported;
+        '  uint32   Partitions;
+        '  string   PNPDeviceID;
+        '  uint16   PowerManagementCapabilities[];
+        '  boolean  PowerManagementSupported;
+        '  uint32   SCSIBus;
+        '  uint16   SCSILogicalUnit;
+        '  uint16   SCSIPort;
+        '  uint16   SCSITargetId;
+        '  uint32   SectorsPerTrack;
+        '  string   SerialNumber;
+        '  uint32   Signature;
+        '  uint64   Size;
+        '  string   Status;
+        '  uint16   StatusInfo;
+        '  string   SystemCreationClassName;
+        '  string   SystemName;
+        '  uint64   TotalCylinders;
+        '  uint32   TotalHeads;
+        '  uint64   TotalSectors;
+        '  uint64   TotalTracks;
+        '  uint32   TracksPerCylinder;
+        '};
+
+        'Button1.Text = InfoHarddisk.diskSerialNumber
+
+
+    End Sub
+    Public Function 硬盘序列号() As String
+        Try
+            Dim myInfo As Microsoft.Win32.RegistryKey = My.Computer.Registry.LocalMachine.OpenSubKey("HARDWARE\DEVICEMAP\Scsi\Scsi Port 0\Scsi Bus 0\Target Id 0\Logical Unit Id 0")
+            硬盘序列号 = Trim(myInfo.GetValue("Identifier")) '这是型号
+        Catch
+            Try
+                Dim myInfo As Microsoft.Win32.RegistryKey = My.Computer.Registry.LocalMachine.OpenSubKey("HARDWARE\DEVICEMAP\Scsi\Scsi Port 1\Scsi Bus 1\Target Id 0\Logical Unit Id 0")
+                硬盘序列号 = Trim(myInfo.GetValue("Identifier"))
+            Catch
+                硬盘序列号 = ""
+            End Try
+        End Try
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim myInfo As Microsoft.Win32.RegistryKey = My.Computer.Registry.LocalMachine.OpenSubKey("HARDWARE\DESCRIPTION\System\MultifunctionAdapter\0\DiskController\0\DiskPeripheral\0")
+        Button2.Text = Trim(myInfo.GetValue("Identifier")) '这个的后半部分是硬盘序列号
+    End Sub
+End Class
